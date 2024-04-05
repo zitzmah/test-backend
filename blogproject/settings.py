5600d8e591c3fcd38326bac747ff0d17299e4478
HZ <haleyzitzmann@gmail.com>
2024-04-05 15:58:36 -0600

    cors stuff added

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -67,6 +67,28 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWS_CREDENTIALS=True
+
+CORS_ALLOW_HEADERS = [
+        'accept',
+        'accept-encoding',
+        'authorization',
+        'content-type',
+        'dnt',
+        'origin',
+        'user-agent',
+        'x-csrftoken',
+        'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
 
 ROOT_URLCONF = 'blogproject.urls'
 
