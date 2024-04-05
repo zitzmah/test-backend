3ef651226fab68005aec9175588e10706eabc745
HZ <haleyzitzmann@gmail.com>
2024-04-05 10:47:42 -0600

    updated models.py

--- blogs/models.py
+++ blogs/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     subject=models.CharField(max_length=200)
-    details=models.CharField(max_length=1000)
-    image=models.CharField(max_length=1000, blank=True, null=True default='app.logomakr.com/3EC6KR')
+    details=models.TextField(max_length=6000)
+    image = models.TextField(max_length=2000, blank=True, null=True, default='app.logomakr.com/3EC6KR')
+
