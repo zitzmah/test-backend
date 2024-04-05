5b907e28e682d7250e97f55db2063ff354b7264e
HZ <haleyzitzmann@gmail.com>
2024-04-05 17:52:54 -0600

    first commit

--- blogs/models.py
+++ blogs/models.py
@@ -4,5 +4,3 @@ from django.db import models
 class Blog(models.Model):
     subject=models.CharField(max_length=200)
     details=models.TextField(max_length=6000)
-    image_url= models.URLField(max_length=6000, blank=True, null=True, default='app.logomakr.com/3EC6KR')
-
