506b4a3f55b8ead7fde1f7687298c39f9c67f1b5
HZ <haleyzitzmann@gmail.com>
2024-04-05 14:39:32 -0600

    updated models.py

--- blogs/models.py
+++ blogs/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Blog(models.Model):
     subject=models.CharField(max_length=200)
     details=models.TextField(max_length=6000)
-    image = models.TextField(max_length=2000, blank=True, null=True, default='app.logomakr.com/3EC6KR')
+    image_url= models.URLField(max_length=6000, blank=True, null=True, default='app.logomakr.com/3EC6KR')
 
--- blogs/serializers.py
+++ blogs/serializers.py
@@ -4,7 +4,4 @@ from rest_framework import serializers
 class BlogSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=Blog
-        fields='__all__'
-        extra_kwargs = {
-            'image_url': {'write_only':True},
-        }
+        fields ='__all__'
